40b63c11440866a8f8846fd9f6a31baa89e65e2e
marchunter <marc.o.j.jaeger@gmail.com>
2017-04-19 19:38:11 +0300

    Add files via upload
    first running version
    -not performance optimised: about 0.3 sec per game (4 players)
    -code not tidied up yet

---
+++ classes_revolution.py
@@ -0,0 +1,233 @@
+# Python classes for the Japanese game "Revolution"
+# The following objects are needed for the game:
+# Match
+# Game
+# Player
+# Deck
+# Card
+# Table
+# Discard
+
+import numpy as np
+
+# helper functions
+
+class Match():
+    """Object with information about game session
+    - Total number of games
+    - Number of games played
+    - Winner, 2nd winner, last and 2nd-to-last of last game
+    - Number of Players
+    - Game mode (variations of revolution)
+    - Play history of each game
+    - Rank history
+    """
+    def __init__(self, n_players, games_total = 12, game_mode = "standard",):
+        self.n_players = n_players
+        self.games_total = games_total
+        self.game_mode = game_mode
+        self.n_games_played = 0
+        self.match_history = []
+        self.player_ranking = []
+        self.rank_history = []
+        return None
+
+    def update(self, player_ranking, game_history,):
+        """
+        Update match after game
+        """
+        self.n_games_played += 1
+        self.player_ranking = player_ranking
+        self.rank_history.append(player_ranking)
+        self.match_history.append(game_history)
+        return None
+
+class Game():
+    """Object with game state information.
+    - Starting Player
+    - Cards played
+    - Play history of game
+    - Revolution switch
+    """
+    def __init__(self, n_players):
+        self.is_rev_switch = False
+        self.game_history = []
+        self.cards_played = []
+        self.starting_player = np.random.randint(0, n_players)
+        return
+
+    def update(self, active_player = "Empty table", cards = []):
+        self.cards_played.append((active_player, cards))
+        return
+
+    def end(self,):
+        """Wraps up the game
+        """
+        self.game_history = self.cards_played
+
+        # delete Discard pile
+
+        # (update match)
+
+        # (update players)
+
+
+        return self.game_history
+
+
+class Player():
+    """Object containing player-specific information
+    - Hand
+    - Rank during previous games
+    - Starting hands
+    """
+    def __init__(self, seating):
+        self.hand = None
+        self.starting_hands = []
+        self.seating = seating # sequencing of players
+        return
+
+    def new_hand(self, hand):
+        self.hand = hand
+        self.starting_hands.append(hand)
+        return 
+
+    def update(self, cards_played):
+        # remove cards from self.hand =
+        # YET TO BE IMPLEMENTED
+        return self.hand
+
+    def clear(self):
+        self.hand = None
+        return None
+
+    def get_ranking(self, match_object):
+        player_ranking_history = match.object.rank_history[:, self.seating]
+
+        return player_ranking_history
+
+
+
+class Deck():
+    """Deck containing card information
+    - Number of cards in deck
+    - Define each card value and suite in the deck on start
+    """
+    def __init__(self, multiplicator = 1, n_jokers = 3):
+        """Creates Deck with (52 cards + jokers) * multiplicator
+        """
+        values = [0,1,2,3,4,5,6,7,8,9,10,11,12,] # 0 = card 3, 12 = card 2
+        suite = [1,2,3,4,] # diamonds 1, hearts 2, spades 3, clubs 4
+        simple_deck = np.array(np.meshgrid(suite, values,)).T.reshape(-1,2)
+        deck_w_jokers = np.vstack((simple_deck, np.array([[0,13],]*n_jokers)))
+        full_deck = np.tile(deck_w_jokers, (multiplicator, 1))
+        self.deck = full_deck
+        return None
+
+    def shuffle(self):
+        np.random.shuffle(self.deck)
+        return self.deck
+
+
+class Table():
+    """Object containing information
+    about the stack
+    - Stack history
+    - Jack switch
+    - Top cards
+    - Empty / not empty
+    - Allowed combination to be played (doubles, triples, revolution, straight)
+    - Suite locked / not locked
+    """
+    def __init__(self):
+        self.is_jack_switch = False
+        self.top_cards = ""
+        self.is_empty = True
+        self.is_suite_lock = False
+        self.stack_history = []
+        self.is_straight = False
+        self.top_player = ""
+
+    def update(self, seating, cards = []):
+        if len(cards) >0:
+            self.top_cards = cards
+
+            self.stack_history.append(cards)
+            self.is_empty = False
+            self.top_player = seating
+        else: 
+            pass
+
+        return self.top_cards
+
+    def clear(self, discard_object):
+        self.is_jack_switch = False
+        self.top_cards = ""
+        self.is_empty = True
+        self.is_suite_lock = False
+        self.is_straight = False
+        self.top_player = ""
+        # update Discard
+        for cards in self.stack_history:
+                discard_object.update(cards)
+        self.stack_history = []
+
+
+class Discard():
+    """Discard pile
+    face down
+    """
+    def __init__(self):
+        self.discard_history = []
+
+    def update(self, cards = None):
+        self.discard_history.append(cards)
+
+    def clear(self):
+        self.discard_history = []
+ 
+
+class Card():
+    """Information about specific card
+    - suite
+    - value
+    Maybe needed for displaying in human-readable format
+    """
+    def __init__(self):
+        self.value_dict = {0 : 3, 1 : 4, 2 : 5, 3 : 6, 4 : 7, 5 : 8, 6 : 9,
+         7 : 10, 8 : "J", 9 : "Q", 10 : "K", 11 : "A", 12 : 2, 13 : "Joker"}
+        self.suite_dict = {0 : "", 1 : "c", 2: "s",
+         3 : "h", 4 : "d"}
+        return None
+
+    def tuple2card(self, card_tuple):
+        card = str(self.value_dict[card_tuple[1]]) + str(self.suite_dict[card_tuple[0]])
+        return card
+
+    def tuples2cards(self, card_tuple_lst):
+        cards = []
+
+        """if len(card_tuple_lst) != 0:
+            if card_tuple_lst.shape[0] == 1:
+                print("DUDE")
+                print(card_tuple_lst)
+                return ""
+                #return self.tuple2card(card_tuple_lst)
+        """
+        for card_tuple in card_tuple_lst:
+            cards.append(self.tuple2card(card_tuple))
+        return cards
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ noprint_revolution.py
@@ -0,0 +1,557 @@
+# Python object-oriented game called "Revolution"
+# Japanese game for any number of players
+# execution of the game
+
+import numpy as np
+from classes_revolution import Match, Game, Player, Deck, Table, Discard, Card
+import itertools, random, time 
+
+### GLOBALS ###
+
+GAME_MODE = "allhuman"
+GAME_MODE = "onehuman"
+GAME_MODE = "allrandom"
+t0 = time.time()
+
+### Helper Functions ### 
+
+def get_human_cards(cards):
+    human_card = Card()
+    human_cards = human_card.tuples2cards(cards)
+    return human_cards
+
+def choose(moves_lst, active_player, mode = GAME_MODE):
+    if GAME_MODE == "onehuman":
+        return choose_0_human_vs_random(moves_lst, active_player)
+    elif GAME_MODE == "allhuman":
+        return choose_action(moves_lst, mode = "human")
+    elif GAME_MODE == "allrandom":
+        return choose_action(moves_lst, mode = "random")
+    else:
+        print("GAME_MODE unknown")
+        return
+
+def choose_0_human_vs_random(moves_lst, active_player):
+    if active_player == 0:
+        #print("human action")
+        return choose_action(moves_lst, mode="human")
+    else:
+        #print("random computer action:")
+        return choose_action(moves_lst, mode="random")
+
+def choose_action(moves_lst, mode="human"):
+    """Choice of action from a list of possible moves
+    """
+    if mode == "human":
+        i = 0
+        for move in moves_lst:
+            #print("Enter ", i, " to select ", get_human_cards(move))
+            i +=1
+        human_choice = input("Enter number: ")
+        try:
+            if 0 <= int(human_choice) <= len(moves_lst):
+                return moves_lst[int(human_choice)]
+        except:
+            pass
+            return np.array([])
+
+        return move
+    elif mode == "random":
+        move = random.choice(moves_lst)
+        return move
+    elif mode == "ml":
+        print("ML not yet implemented")
+        return
+    else:
+        print("Mode not known. Modes are 'human', 'random' and 'ml'")
+        return
+
+
+
+# initiate Match
+# define game mode
+# define number of players
+# define sequence of players
+
+current_match = Match(4, 100)
+
+# initiate Players
+player_lst = []
+for seat in range(0, current_match.n_players):
+    player = Player(seat)
+    player_lst.append(player)
+
+# initiate empty Table
+
+table = Table()
+
+# initiate empty Discard
+
+discard_pile = Discard()
+
+# repeat until total number of games is reached
+while current_match.games_total !=  current_match.n_games_played:
+
+    # initiate Game #
+    #################
+    #print("Game " + str(current_match.n_games_played + 1) + " is starting!")
+
+    current_game = Game(current_match.n_players)
+
+    # create deck with size according to the number of players
+
+    new_deck = Deck()
+
+
+    # shuffle deck
+    shuffled_deck = new_deck.shuffle()
+
+    # distribute cards to hands of players, remaining cards are discarded (face-down)
+    deck_size = shuffled_deck.shape[0]
+    cards_per_player = deck_size // current_match.n_players # integer division
+    remaining_cards = deck_size - cards_per_player * current_match.n_players 
+
+        # update Players
+    i = 0
+    for player in player_lst:
+        j = i + cards_per_player
+        player.new_hand(shuffled_deck[i:j])
+        i = j
+
+        # update Discard
+    discard_pile.update(shuffled_deck[j:])
+
+
+
+    # Exchange of cards between players
+
+    #example rank #
+    current_match.player_ranking = np.array([0,1,3,2])
+    ##
+
+    if str(current_match.player_ranking) == "":
+        pass
+    else:
+        first_place, second_place, second_to_last_place, last_place = current_match.player_ranking[[0,1,-2,-1]]
+        first_hand = player_lst[first_place].hand
+        second_hand = player_lst[second_place].hand
+        second_to_last_hand = player_lst[second_to_last_place].hand
+        last_hand = player_lst[last_place].hand
+
+        # first player and last player
+        first_arg = np.argsort(first_hand[:,1])
+        first_cards = np.copy(first_hand[first_arg[:2]]) # two worst cards
+
+        last_arg = np.argsort(last_hand[:,1])
+        last_cards = np.copy(last_hand[last_arg[-2:]]) # two best cards
+
+        # swap cards
+        first_hand[first_arg[:2]] = last_cards
+        last_hand[last_arg[-2:]] = first_cards
+        ##print("first and last swapped cards:", first_cards, last_cards)
+
+        # second player and second-to-last player
+        second_arg = np.argsort(second_hand[:,1])
+        second_card = np.copy(second_hand[second_arg[:1]]) # worst card
+
+        second_to_last_arg = np.argsort(second_to_last_hand[:,1])
+        second_to_last_card = np.copy(second_to_last_hand[second_to_last_arg[-1:]]) # best card
+
+        # swap cards
+        second_hand[second_arg[:1]] = second_to_last_card
+        second_to_last_hand[second_to_last_arg[-1:]] = second_card
+        ##print("second and second to last swapped cards:", 
+        #   second_card, second_to_last_card)
+
+
+
+    # (small action required) # currently ignored
+
+    # update Player (has automatically taken place due to referencing numpy array)
+
+    # define starting player (first game random, otherwise last player begins) ? check with rules ?!?
+    if current_match.n_games_played == 0:
+        starting_player = player_lst[current_game.starting_player]
+    else:
+        starting_player = player_lst[current_match.player_ranking[-1]]
+
+    current_ranking = []    # list of players in sequence of having finished
+    active_player_seating = starting_player.seating
+    active_player = player_lst[active_player_seating]
+
+    ### Start of turns ###
+    #####################
+
+    # repeat until game is over
+    while len(current_ranking) !=  current_match.n_players:
+
+        #print("##### Turn of Player " + str(active_player_seating) + " #####")
+
+    # Check possible moves
+    # includes suite lock, doubles, triples, quadruples, revolution, straight
+
+        active_hand = active_player.hand
+        #print("Active Hand")
+        #print(get_human_cards(active_hand))
+        ##print(active_hand)
+
+        arg = np.argsort(active_hand[:,1])
+        active_hand = active_hand[arg]
+        diff = active_hand - np.insert(active_hand[:-1], 0, active_hand[0], axis = 0)
+
+        # split into same value pools
+        split_hand = np.split(active_hand, np.argwhere(diff[:,1] != 0.0).flatten()[0:])
+
+        # calculate possible moves:
+        
+        # 1) Single, Double, Triple, ...
+        possible_sames = [np.array([])]
+        for same_value in split_hand:
+            index_lst = list(itertools.product([False, True], repeat=same_value.shape[0]))
+            ##print("product list", index_lst)
+            index_array = np.array(index_lst)
+
+            for indices in index_array:
+                move = same_value[indices,:]
+                ##print(move)
+                if str(move) != "[]":
+                    possible_sames.append(move)
+        ##print("possible moves Single, Double, Triple, ...", possible_sames)
+        
+        # 2) Straights
+        # remove duplicates (cards with same suite and value)
+        b = np.ascontiguousarray(active_hand).view(np.dtype((np.void, active_hand.dtype.itemsize * active_hand.shape[1])))
+        _, idx = np.unique(b, return_index=True)
+        uniques = active_hand[idx]
+
+        # split into same suite pools
+
+        arg = np.argsort(uniques[:,0])
+        active_unique_hand = uniques[arg]
+        diff = active_unique_hand - np.insert(active_unique_hand[:-1], 0, active_unique_hand[0], axis = 0)
+        split_hand = np.split(active_unique_hand, np.argwhere(diff[:,0] != 0.0).flatten()[0:])
+
+        # find neighbouring values
+        possible_straights = []
+
+        for same_suite in split_hand:
+            ##print(same_suite.shape[0])
+            if same_suite.shape[0] >= 3:
+
+                arg = np.argsort(same_suite[:,1])
+                same_suite = same_suite[arg]
+                diff = same_suite - np.insert(same_suite[:-1], 0, same_suite[0] -1, axis = 0)
+
+                ##print("same suite cards:", same_suite)
+
+                split_suite = np.split(same_suite, np.argwhere(diff[:,1] != 1.0).flatten()[0:])
+                ##print("split suite")
+                ##print(split_suite)
+                for straight in split_suite:
+                    if straight.shape[0] >= 3:
+
+                        # calculate possible moves:
+                        # Straight
+                        index_lst = list(itertools.product([False, True], repeat=straight.shape[0]))
+                        ##print("product list", index_lst)
+                        index_array = np.array(index_lst)
+
+                        for indices in index_array:
+                            move = straight[indices,:]
+                            if move.shape[0] >= 3: 
+                                diff = move - np.insert(move[:-1], 0, move[0] -1, axis = 0)
+                                ##print("same suite cards:", same_suite)
+                                if np.all(diff[:,1] == 1):                              
+                                    possible_straights.append(move)
+        ##print("possible straight moves", possible_straights)
+
+        # unite moves
+        possible_moves = possible_sames + possible_straights
+
+        ### INFO SECTION ###
+        #print("is table empty?", table.is_empty)
+        #print("active player", active_player_seating)
+        #print("is straight?", table.is_straight)
+        #print("is revolution?", current_game.is_rev_switch)
+        #print("is jack switch?", table.is_jack_switch)
+        #print("is suite lock?", table.is_suite_lock)
+        #print("top player", table.top_player)
+        #print("top cards", get_human_cards(table.top_cards))
+        #####################
+
+        # check which of the possible moves are allowed considering the table
+        if table.is_empty == True:
+            possible_moves = possible_moves
+        # if table is not empty
+        else:
+            # straight
+            if table.is_straight == True:
+                #print("Straight")
+                possible_moves = possible_straights
+            # single, double, triple, ...
+            else:
+                possible_moves = possible_sames
+
+            # number of cards on top
+            tmp_moves = []
+            for move in possible_moves:
+                if move.shape[0] == table.top_cards.shape[0]:
+                    tmp_moves.append(move)
+            possible_moves = tmp_moves
+            ##print("moves with correct number of cards:", possible_moves)
+
+            # check equality of suite with top cards if necessary
+            if table.is_suite_lock:
+                tmp_moves = []
+                for move in possible_moves:
+
+                    arg = np.argsort(move[:,0])
+                    move = move[arg]
+                    arg = np.argsort(table.top_cards[:,0])
+                    table.top_cards = table.top_cards[arg]
+                    if np.all(move[:,0] == table.top_cards[:,0]):
+                        tmp_moves.append(move)  
+                possible_moves = tmp_moves
+                ##print("Suite lock moves possible:")
+                ##print(possible_moves)
+
+            # check wether the possible moves have a higher value
+            tmp_moves = []
+            for move in possible_moves:
+                move_max = np.max(move[:,1])
+                top_cards_max = np.max(table.top_cards[:,1])
+                # if greater than 0, then move is possible
+                difference = move_max - top_cards_max
+                if table.is_jack_switch != current_game.is_rev_switch:
+                    ##print("Reverse order")
+                    difference = - difference
+                else:
+                    ##print("Normal order")
+                    pass
+                    # -13 for only 3 can top joker, joker can go under 3
+                if difference > 0 or difference == -13:
+                    tmp_moves.append(move)
+
+            possible_moves = tmp_moves
+
+            # append "Pass"
+            possible_moves.append(np.array([]))
+            ##print("Moves possible:")
+            ##print(possible_moves)
+
+    ## Action of players ##
+    #######################
+    # Action: Choose move or pass
+        #print("ACTION!")
+        move = choose(possible_moves, active_player_seating)
+
+        ##print("ACTION DONE")
+        #print("It has been chosen to play ", get_human_cards(move))
+
+        
+
+    # update Player
+        # remove cards from hand
+        if len(move) > 0:
+            for card in move:
+                arg = np.argmin(np.any(active_hand[:] != card, axis = 1))
+                # all true
+                all_but_one = np.any(active_hand[:] != 42, axis = 1)
+                # except first appearance of card
+                all_but_one[arg] = False
+                active_hand = active_hand[all_but_one]
+                #active_hand = active_hand[np.any(active_hand[:] != card, axis = 1)]
+                active_player.hand = active_hand
+    # update Game
+
+        current_game.update(active_player_seating, move)
+
+    # update Table
+
+        # add top cards
+        if len(move) > 0:
+            # check suite lock condition
+            if table.is_empty == True:
+                table.is_empty == False
+            elif table.is_suite_lock == False and len(move) > 1: 
+                arg = np.argsort(move[:,0])
+                move = move[arg]
+                arg = np.argsort(table.top_cards[:,0])
+                table.top_cards = table.top_cards[arg]
+                if np.all(move[:,0] == table.top_cards[:,0]):
+                    table.is_suite_lock == True
+
+            table.update(active_player_seating, move,)
+
+            # check straight condition
+            if table.is_straight == False and len(move) >=3:
+                arg = np.argsort(move[:,1])
+                move = move[arg]
+                diff = move - np.insert(move[:-1], 0, move[0] -1, axis = 0)
+                if np.all(diff[:,1] == 1):
+                    table.is_straight = True
+
+
+    ### Special cards and revolution ###
+    # if revolution is played
+        # update Game
+            if len(move) >= 4:
+                current_game.is_rev_switch = not current_game.is_rev_switch
+    # if 10 is played:
+            # discard one card for every 10
+            for boolean in table.top_cards[:,1] == 7:
+                # the last card can't be discarded
+                if boolean and active_player.hand.shape[0] > 1:        
+            # Action: Choose card in Hand
+                    #print("Please discard a card")
+                    discard_card_list = []
+                    for card in active_hand:
+                        discard_card_list.append([card])
+
+                    discarded_card = choose(discard_card_list, active_player_seating)
+                    #discarded_card = choose(active_hand, active_player_seating)
+                    #print("Player discarded:", get_human_cards(discarded_card))
+    ## End of Action ##
+    ###################
+            # update Player 
+                    active_hand = active_hand[np.any(active_hand[:] != discarded_card, axis = 1)]
+                    active_player.hand = active_hand
+
+            # update Discard
+                    discard_pile.update(discarded_card)
+
+    # if Jack is played:
+            if np.any(table.top_cards[:,1] == 8):
+        # update Table
+                table.is_jack_switch = not table.is_jack_switch
+
+    # if 8 is played:
+            if np.any(table.top_cards[:,1] == 5):
+        # update Table
+                table.clear(discard_pile)
+                current_game.update()
+        # update Game (starting player)
+                if len(active_hand) > 0:
+                    current_game.starting_player = active_player
+                    # active player may play again
+                    continue
+    ###
+
+    ### Updating the game status ###
+    # check if Hand of one Player is empty
+            if len(active_player.hand) == 0:
+                #print("player finished", active_player_seating)
+                current_ranking.append(active_player_seating)
+
+
+        # repeat until all passed
+        elif len(move) == 0:
+            # check if all passed including player with top cards
+            # empty the Table, player who played the last cards becomes active player
+            if table.top_player == active_player_seating:
+                #print("empty table for player:", active_player_seating)
+                # update Table
+                table.clear(discard_pile)
+                current_game.update()
+                continue
+            # check now wether the player is already done
+            while table.top_player in current_ranking:
+                table.top_player +=1
+                if table.top_player == current_match.n_players:
+                    table.top_player = 0
+
+        while True:
+            # next player's turn
+            active_player_seating += + 1
+            if active_player_seating == current_match.n_players:
+                active_player_seating = 0
+            active_player = player_lst[active_player_seating]
+            # exclude finished players
+            if active_player_seating in current_ranking:
+                #print("skip player", active_player_seating)
+                continue
+            else:
+                break
+
+        # repeat until one Player is left
+        if len(current_ranking) ==  current_match.n_players- 1:
+            #print("Game has ended.")
+            #print("Last player:", active_player_seating)
+            current_ranking.append(active_player_seating)
+            #print("Game ranking:", current_ranking)
+            break
+    ###
+
+    # end of game - update
+
+    # Table
+    table.clear(discard_pile)
+
+    # Game
+
+    current_game.end()
+
+    # Match
+
+    current_match.update(current_ranking, current_game.game_history)
+
+    # Players
+
+    for player in player_lst:
+        player.clear()
+
+    # update Discard
+
+    discard_pile.clear()
+
+    # End of Game #
+    ###############
+    #print("Game " + str(current_match.n_games_played) + " completed")
+
+    # repeat Game until total number of games is reached
+
+# End of Match #
+################
+#print("Match has ended!")
+
+# Anounce ranking and winner
+#print("Ranking:", current_match.rank_history)
+
+# display match stats
+#print("Match history", current_match.match_history)
+
+all_rankings = np.array(current_match.rank_history)
+first_places = np.bincount(all_rankings[:,0])
+total_ranking = np.flipud(np.argsort(first_places))
+winner = np.where(first_places[:] == np.max(first_places[:]))
+
+#winner = np.argmax(first_places)
+#print("Total ranking: " + str(total_ranking))
+#print("The winner is player:", str(winner))
+
+
+# FINALISE #
+t1 = time.time()
+
+print("Total run time:", t1-t0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
